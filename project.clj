403114c1a8bb60a9e3cd5874c829742b3b5e7a90
ag91 <agiugliano@live.it>
2017-12-09 23:56:15 +0100

    Add initial configuration

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject reactive-programming-tutorial-with-beicon "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:license {:name "LGPL"
+                      :url "https://www.gnu.org/licenses/lgpl-3.0.txt"}}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [funcool/beicon "4.1.0"]])
